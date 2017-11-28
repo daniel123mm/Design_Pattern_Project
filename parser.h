5e12ffa3f366c59423b622efc5ab1fcc98f84d7d
Daniel Liu <danielliu@muchtel.com>
2017-11-28 21:29:57 +0800

    update

--- parser.h
+++ parser.h
@@ -25,8 +25,7 @@ public:
     if(token == VAR){
 		Term *variable = new Variable(symtable[_scanner.tokenValue()].first);
 		if(sign == 1){
-			if(_findTerms.size() < 3){
-				for(int i = (int)_findTerms.size() - 1;i > (int)_findTerms.size() - 3;i--){
+        for(int i = 0;i < (int)_findTerms.size();i++){
 					Struct *st = dynamic_cast<Struct *> (_findTerms[i]);
 					if(st){
 						variable = findStructArgs(st,variable);
@@ -34,21 +33,9 @@ public:
 						if(_findTerms[i]->symbol() == variable->symbol()){
 							variable = _findTerms[i];
 						}
-					}
-				}
-			}else{
-				for(int i = (int)_findTerms.size() - 1;i > (int)_findTerms.size() - 4;i--){
-					Struct *st = dynamic_cast<Struct *> (_findTerms[i]);
-					if(st){
-						variable = findStructArgs(st,variable);
-					}else{
-						if(_findTerms[i]->symbol() == variable->symbol()){
-							variable = _findTerms[i];
-						}
-					}
-				}
-			}
-		}
+          }
+        }
+     }
       return variable;
     }else if(token == NUMBER){
       return new Number(_scanner.tokenValue());
@@ -66,8 +53,6 @@ public:
 
     return nullptr;
   }
-
-
 
   Term * structure() {
     Atom structName = Atom(symtable[_scanner.tokenValue()].first);
@@ -105,30 +90,13 @@ void matchings(){
     while(_scanner.currentChar() != '.') {
      Term * term = createTerm();
      if ( term != nullptr ){
-		 /*
-		if(sign == 1){
-			for(int i = (int)_terms.size() - 1;i > (int)_terms.size() - 3;i--){
-				Struct *st = dynamic_cast<Struct *> (_terms[i]);
-				if(st){
-					//findStructArgs(st,term);
-					for(int j = 0;j < st->arity();j++){
-						if(st->args(j)->symbol() == term->symbol()){
-							term = st->args(j);
-							break;
-						}
-					}
-					
-				}else{
-					if(_terms[i]->symbol() == term->symbol()){
-						term = _terms[i];
-					}
-				}
-			}
-		}
-		*/
-		Node * n = new Node(TERM, term, nullptr, nullptr);
-		_terms.push_back(term);
-		_findTerms.push_back(term);
+       if(sign == 2){
+         _findTerms.clear();
+         sign = 0;
+       }
+		    Node * n = new Node(TERM, term, nullptr, nullptr);
+		    _terms.push_back(term);
+		    _findTerms.push_back(term);
         op_term.push(n);
       }
      if( _scanner.currentChar() == '=' ) {
@@ -136,18 +104,18 @@ void matchings(){
         op_op.push(n);
      }
      if( _scanner.currentChar() == ',' ) {
-		sign = 1;
+		    sign = 1;
         Node * n = new Node(COMMA);
         Node * temp = makeTree();
-		op_term.push(temp);
-		op_op.push(n);
+		    op_term.push(temp);
+		    op_op.push(n);
      }
-	 if( _scanner.currentChar() == ';' ) {
-		sign = 2;
+	  if( _scanner.currentChar() == ';' ) {
+		    sign = 2;
         Node * n = new Node(SEMICOLON);
         Node * temp = makeTree();
-		op_term.push(temp);
-		op_op.push(n);
+		    op_term.push(temp);
+		    op_op.push(n);
      }
    }
     Node * temp ;
@@ -160,15 +128,14 @@ void matchings(){
   
   Term* findStructArgs(Struct *s, Term *v){
 	   for(int j = 0;j < s->arity();j++){
-			if(Struct *st = dynamic_cast<Struct *> (s->args(j))){
-				return findStructArgs(st, v); 
-			}else{
-				if(s->args(j)->symbol() == v->symbol()){
-					return s->args(j);
-				}
-			}
-	   }
-	  
+			  if(Struct *st = dynamic_cast<Struct *> (s->args(j))){
+				  return findStructArgs(st, v); 
+			  }else{
+			  	if(s->args(j)->symbol() == v->symbol()){
+					  return s->args(j);
+				  }
+			  }
+	  }  
   }
 
   Node * makeTree() {
